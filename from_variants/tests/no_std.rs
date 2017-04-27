9c922b60a4e84c55d21701ce55720a2b83861c54
Ted Driggs <ted.driggs@outlook.com>
2017-04-27 09:03:24 -0700

    Integration tests for `no_std` support

---
+++ from_variants/tests/no_std.rs
@@ -0,0 +1,21 @@
+//! Compile and correctness test for using `core` instead of `std`.
+#![no_std]
+
+#[macro_use]
+extern crate from_variants;
+
+#[derive(Debug, PartialEq, Eq, FromVariants)]
+pub enum Lorem {
+    Ipsum(u8),
+    Dolor(i32),
+}
+
+#[test]
+fn from_u8() {
+    assert_eq!(Lorem::Ipsum(1), Lorem::from(1u8));
+}
+
+#[test]
+fn from_i32() {
+    assert_eq!(Lorem::Dolor(-1), Lorem::from(-1));
+}
